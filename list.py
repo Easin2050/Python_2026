9d4a92062903e5755a894c4af043db41c341a109
easin <easin562050@gmail.com>
2026-05-12 23:16:54 +0600

    9 problem solve done

--- list.py
+++ list.py
@@ -1,26 +1,94 @@
-fruits=['banana','orange','mango','lemon']
-print('Fruits:', fruits)
-print('Length of the list:',len(fruits))
-lst = ['Asabeneh', 250, True, {'country':'Finland', 'city':'Helsinki'}] # list containing different data types
-print('List:', lst)
-print('First element:', lst[0])
-print('Last element:', lst[-1])
 
-lst = ['item1','item2','item3', 'item4', 'item5']
-first_item, second_item, third_item, *rest = lst
-print(first_item)  
-print(second_item)
-print(third_item)  
-print(rest)
+"""
+Declare an empty list
 
-countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
-gr, fr, bg, sw, *scandic, es = countries
-print(gr) 
-print(fr)
-print(bg)
-print(sw)
-print(scandic)
-print(es)
+Declare a list with more than 5 items
 
-all_fruits=fruits[::2]
-print(all_fruits)
+Find the length of your list
+
+Get the first item, the middle item and the last item of the list
+
+Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+
+Print the list using print()
+
+Print the number of companies in the list
+
+Print the first, middle and last company
+
+Print the list after modifying one of the companies
+
+Add an IT company to it_companies
+
+Insert an IT company in the middle of the companies list
+
+Change one of the it_companies names to uppercase (IBM excluded!)
+
+Join the it_companies with a string '#;  '
+
+Check if a certain company exists in the it_companies list.
+
+Sort the list using sort() method
+
+Reverse the list in descending order using reverse() method
+
+Slice out the first 3 companies from the list
+
+Slice out the last 3 companies from the list
+
+Slice out the middle IT company or companies from the list
+
+Remove the first IT company from the list
+
+Remove the middle IT company or companies from the list
+
+Remove the last IT company from the list
+
+Remove all IT companies from the list
+
+Destroy the IT companies list
+
+Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack, then insert Python and SQL after Redux.
+
+Exercises: Level 2
+The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+Sort the list and find the min and max age
+Add the min age and the max age again to the list
+Find the median age (one middle item or two middle items divided by two)
+Find the average age (sum of all items divided by their number )
+Find the range of the ages (max minus min)
+Compare the value of (min - average) and (max - average), use abs() method
+Find the middle country(ies) in the countries list
+Divide the countries list into two equal lists if it is even if not one more country for the first half.
+['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic coutries"""
+
+# Declare an empty list
+empty_list=[]
+
+#2 Declare a list with more than 5 items
+empty_list=[1,2,3,4,5,6]
+#3 Find the length of your list
+print(len(empty_list))
+#4 Get the first item, the middle item and the last item of the list
+print(empty_list[0])
+print(empty_list[len(empty_list)//2])
+print(empty_list[-1])
+
+#5 Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+user_info=['John',30,5.6,'single','abc street,Dhaka']
+print(user_info)
+#6 Declare a list variable named it_companies and assign inital values Facebook,Google,Microsoft,Apple,IBM,Oracle and Amazon
+it_companies=['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+
+#7 Print the list using print()
+print(it_companies)
+#8 Print the number of companies in the list
+print(len(it_companies))
+#9 Print the first, middle and last company
